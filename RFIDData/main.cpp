f98b2741693d61cfe89208f68251cb5cc6c6d728
chris-schmitz <schmitz.chris@gmail.com>
2019-02-17 15:30:52 -0600

    Changes class to struct (everything's public now), removes non-arduino friendly stuff from the struct (assuming, still need to test), adds some comparison logic, and adds a way of clearing out the id.

--- RFIDData/main.cpp
+++ RFIDData/main.cpp
@@ -1,54 +1,59 @@
 #include <iostream>
 
-class RFIDData {
-public:
+struct RFIDData {
     static const uint8_t idLength = 10;
     int idArray[idLength] = {0};
-    
-private:
     uint8_t currentIndex = 0;
-    
-public:
+
     void addIdElement(int element) {
         this->idArray[this->currentIndex] = element;
         this->currentIndex++;
     }
-    
-    
-    void printArray() {
-        for (uint8_t i = 0 ; i < idLength; i ++){
-            std::cout << std::hex << this->idArray[i];
-        }
-        std::cout << std::endl;
-    }
-    
-    
+
     bool compare( RFIDData payload2){
+        if (this->currentIndex != payload2.currentIndex){
+            return false;
+        }
+        
         for (uint8_t i = 0 ; i < this->currentIndex; i++){
             if (this->idArray[i] != payload2.idArray[i]) {
                 return false;
             }
         }
+        
         return true;
+    }
+    
+    void clearId () {
+        for (uint8_t i = 0 ; i < this->idLength; i++) {
+            this->idArray[i] = 0;
+        }
+        this->currentIndex = 0;
     }
 };
 
 int idArray[] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x11, 0x22, 0x33};
+int idArray2[] = {0xa2, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x11, 0x22, 0x33};
 
 int main(int argc, const char * argv[]) {
 
     RFIDData rfid1;
     RFIDData rfid2;
     
-    for (uint8_t i = 0; i < 10; i++){
+    for (uint8_t i = 0; i < (sizeof(idArray) / sizeof(int)); i++){
         rfid1.addIdElement(idArray[i]);
     }
-    rfid1.printArray();
-    
-    for (uint8_t i = 0; i < 9; i++){
-        rfid2.addIdElement(idArray[i]);
+
+    for (uint8_t i = 0; i < (sizeof(idArray2) / sizeof(int)); i++){
+        rfid2.addIdElement(idArray2[i]);
     }
-    rfid2.printArray();
-    
+
     std::cout << rfid1.compare(rfid2) << std::endl;
+    
+    rfid2.clearId();
+    
+    for (int i = 0 ; i < 10; i++) {
+        std::cout << rfid2.idArray[i] << " ";
+    }
+    
 }
